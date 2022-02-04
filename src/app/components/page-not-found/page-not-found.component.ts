496352971efd7972002a2c711eb57bb56e3bc3fa
Love Beling <love.beling@se.experis.com>
2022-02-04 13:17:56 +0100

    feat(404 page): added buttons to navigate back to login or trainer page depending on if the user is logged in

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
+  constructor(private readonly router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  UserExistInLocalStorage(): boolean {
+    if (localStorage.getItem('user')) return true;
+    else return false;
   }
 
+  OnClickBackToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  OnClickBackToTrainerPage(): void {
+    this.router.navigate(['/trainer']);
+  }
 }
