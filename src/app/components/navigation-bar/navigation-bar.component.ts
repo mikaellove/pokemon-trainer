7201171b3fd70df08e7904f5d75d93624afa287a
Andreas Hellström <andreas.hellstrom@se.experis.com>
2022-02-07 16:06:29 +0100

    feat(navlinks): contitionally render the navlinks if a user is logged in or not

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user-model';
+import { getUser } from 'src/app/utils/storage';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get username(): any {
+    const user: UserModel = getUser();
+
+    if (user) {
+      return user.username;
+    }
+    return null;
   }
-
 }
